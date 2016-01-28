e87ae8beed1625768acff74fed0068045d064154
oleg <obutovich@gmail.com>
2016-01-28 17:50:03 +0100

    use java in background

--- index.js
+++ index.js
@@ -10,33 +10,41 @@ PluginError = gutil.PluginError;
 
 PLUGIN_NAME = 'gulp-plantuml';
 
-module.exports = function(options) {
+module.exports = function (options) {
+
     var args, cmnd, ref;
     if (options == null) {
         options = {};
     }
+
     cmnd = 'java';
-    args = ['-jar'];
+    args = ['-Djava.awt.headless=true', '-jar'];
     args.push((ref = options.jarPath) != null ? ref : "plantuml.jar");
     args.push('-p');
     if (options.format === 'svg') {
         args.push('-tsvg');
     }
-    return through.obj(function(file, encoding, callback) {
+
+    return through.obj(function (file, encoding, callback) {
+
         var b, eb, ext, original_file_path, program;
         if (file.isNull()) {
             return callback(null, file);
         }
+
         if (file.isStream()) {
             return callback(new PluginError(PLUGIN_NAME, 'Streaming not supported'));
         }
+
         original_file_path = file.path;
         ext = options.format === 'svg' ? '.svg' : '.png';
         file.path = gutil.replaceExtension(file.path, ext);
         program = spawn(cmnd, args);
         b = new Buffer(0);
         eb = new Buffer(0);
-        program.stdout.on('readable', function() {
+
+        program.stdout.on('readable', function () {
+
             var chunk, results;
             results = [];
             while (chunk = program.stdout.read()) {
@@ -44,11 +52,15 @@ module.exports = function(options) {
             }
             return results;
         });
-        program.stdout.on('end', function() {
+
+        program.stdout.on('end', function () {
+
             file.contents = b;
             return callback(null, file);
         });
-        program.stderr.on('readable', function() {
+
+        program.stderr.on('readable', function () {
+
             var chunk, results;
             results = [];
             while (chunk = program.stderr.read()) {
@@ -56,7 +68,9 @@ module.exports = function(options) {
             }
             return results;
         });
-        program.stderr.on('end', function() {
+
+        program.stderr.on('end', function () {
+
             var err, msg;
             if (eb.length > 0) {
                 err = eb.toString();
@@ -64,7 +78,9 @@ module.exports = function(options) {
                 return callback(new PluginError(PLUGIN_NAME, msg));
             }
         });
-        return program.stdin.write(file.contents, function() {
+        
+        return program.stdin.write(file.contents, function () {
+
             return program.stdin.end();
         });
     });
